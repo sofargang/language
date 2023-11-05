820e3351516f7943a6895ccccf7830cf7d8cae31
sofargang <sofargang@gmail.com>
2023-11-06 00:42:07 +0900

    no message

--- coding_test/list/1406.cpp
+++ coding_test/list/1406.cpp
@@ -5,30 +5,42 @@ int main(void){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	list<int> l;
-	string input;
-	cin >> input;
+	list<char> l;
+	string inputString;
+	cin >> inputString;
 
-	for(int i=0;i<input.length();i++)
-		l.push_back(input[i]);
+	for(int i=0;i<inputString.length();i++)
+		l.push_back(inputString[i]);
 
+	//l.push_back(' ');
+	list<char>::iterator pos = l.end();
+	
 	int inputCount;
 	char inputChar;
 	cin >> inputCount;
-
 	for(int i=0;i<inputCount;i++){
 		cin >> inputChar;
 		switch(inputChar){
 			case 'P':
 				cin >> inputChar;
+				l.insert(pos, inputChar);
+				break;
+			case 'L':
+				if(pos != l.begin()) --pos;
 				break;
 			case 'D':
+				if(pos != l.end()) ++pos;
 				break;
 			case 'B':
-				break;
-			default:
+				if(pos != l.begin()){
+					pos = l.erase(--pos);
+
+				}
 				break;
 		}
 	}
-	cout << input;
+	
+	for(auto c : l)
+		cout << c;
+	cout << endl;
 }
